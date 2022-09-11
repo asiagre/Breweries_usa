53ee46bb37adc5ab368674223db8f212cc4eaba2
Joanna Zawiślak <asia.greczka@gmail.com>
2022-09-11 11:58:03 +0200

    Create repository

---
+++ src/main/java/com/example/breweries_us/BreweriesUsApplication.java
@@ -0,0 +1,13 @@
+package com.example.breweries_us;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweriesUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweriesUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/breweries_us/BreweriesUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.breweries_us;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweriesUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
